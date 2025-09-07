0951c534f58f9be8bf7ccc27272588d036180f98
cispe <cispe@CISPE-DESKTOP>
2025-09-07 15:50:14 +0300

    Add project files.

---
+++ GraphViewer.BlazorApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphViewer.BlazorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
